5906a174b0e853934c2b42c743f8c1b4cdaef5e5
Andrew Bowen <andrew.james.bowen@gmail.com>
2019-03-13 21:44:54 +1100

    Physics working

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -1,25 +1,32 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody _rigidbody;
 
-    public float Force = 100f;
+    public float Force = 1000f;
+    public float RotationForce = 10000f;
 
     void Start()
     {
         _rigidbody = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
     void Update()
     {
         if (Input.GetKey(KeyCode.Space))
         {
-            _rigidbody.AddForce(transform.right * Force * Time.deltaTime);
-            Debug.Log("Pressed");
+            _rigidbody.AddForce(Vector3.left * Force * Time.deltaTime);
+        }
+
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            _rigidbody.AddRelativeTorque(Vector3.forward * RotationForce * Time.deltaTime);
+        }
+
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            _rigidbody.AddRelativeTorque(Vector3.back * RotationForce * Time.deltaTime);
         }
     }
 }
