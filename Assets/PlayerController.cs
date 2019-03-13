ed3616afd702be6a032bd56c5d923e125d785f75
Andrew Bowen <andrew.james.bowen@gmail.com>
2019-03-13 21:10:00 +1100

    Initial commit with broken Scene camera

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody _rigidbody;
+
+    public float Force = 100f;
+
+    void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            _rigidbody.AddForce(transform.right * Force * Time.deltaTime);
+            Debug.Log("Pressed");
+        }
+    }
+}
