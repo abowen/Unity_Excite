c73b9e78a96c6dc8f05bfa407587c8fb72c339eb
Andrew Bowen <andrew.james.bowen@gmail.com>
2019-03-17 21:17:21 +1100

    Finished 2.5D snowboarding

---
+++ Assets/Player2DController.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class Player2DController : MonoBehaviour
+{
+    private Rigidbody _rigidbody;
+
+    public float Force = 1000f;
+    public float RotationForce = 10000f;
+
+    void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            _rigidbody.AddForce(Vector3.left * Force * Time.deltaTime);
+        }
+
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            _rigidbody.AddRelativeTorque(Vector3.forward * RotationForce * Time.deltaTime);
+        }
+
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            _rigidbody.AddRelativeTorque(Vector3.back * RotationForce * Time.deltaTime);
+        }
+    }
+}
